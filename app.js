a4188d3cd50cdd921c20bdb23d4eae4001083c31
Alvaro <alvaromagu2002@gmail.com>
2022-03-26 11:26:13 +0100

    - users route added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // routers
 const indexRouter = require('./routes/index');
+const usersRouter = require('./components/users');
 
 app.use('/', indexRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
