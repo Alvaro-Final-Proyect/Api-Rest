62171af8f2c601a4de64523900860acdbe736701
Alvaro <alvaromagu2002@gmail.com>
2022-03-26 16:33:22 +0100

    - login working

---
+++ routes/login.js
@@ -0,0 +1,29 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+const login = (request, response) => {
+    const username = request.body.username;
+    const email = request.body.email;
+
+    User.findOne(
+        { 
+            $or: [
+                { 'username': { $regex : new RegExp(username, "i") } },
+                { 'email': { $regex : new RegExp(email, "i") } }
+            ]
+        },
+        async (error, user) => {
+            if(error) return response.status(500).send({ message: error });
+            if(!user) return response.status(404).send({ message: 'User not found' });
+            request.user = user;
+            if(!await bcrypt.compare(request.body.password, user.password)){
+                return res.status(401).json({ message: 'Invalid password' });
+            }
+            response.status(200).send({
+                message: `You logged in correctly, welcome ${user.name}`
+            });
+        }
+    );
+}
+
+module.exports = login;
