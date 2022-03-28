2c6bbed0e475434bd75aa8d3e5bbb43d26417489
Alvaro <alvaromagu2002@gmail.com>
2022-03-28 21:56:40 +0200

    - register route

---
+++ routes/register.js
@@ -0,0 +1,31 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+const register = async (request, response) => {
+    const hashedPassword = await bcrypt.hash(request.body.password, 10);
+
+    const user = new User({
+        _id: new ObjectId(),
+        username: request.body.username,
+        email: request.body.email,
+        password: hashedPassword,
+        name: request.body.name,
+        surname: request.body.surname,
+        gender: request.body.gender,
+        level: request.body.level,
+        friends: [],
+        friendsRequests: []
+    });
+
+    try{
+        await user.save();
+        user.password = '';
+        response.status(201).json(user);
+    }catch(error){
+        console.error(error.message);
+        response.status(400).json({ message: error.message });
+    }
+}
+
+module.exports = register;
