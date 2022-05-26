4c1686716a0d4ea61c1acb50988d21c95e3b5790
Alvaro <alvaromagu2002@gmail.com>
2022-05-26 19:24:08 +0200

    - .env config

--- components/users/routes/getUserMatches.js
+++ components/users/routes/getUserMatches.js
@@ -2,7 +2,11 @@ const Match = require('../../../models/match');
 
 const getUserMatches = async (request, response) => {
     const userId = request.userId;
-    const matches = await Match.find({players: userId});
+    const matches = await Match.find({players: userId}).populate({
+        path: "players",
+        model: "User",
+        retainNullValues: true 
+    });
 
     response.json(matches);
 }
--- mongoose/index.js
+++ mongoose/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/dbpadel', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGOOSE_CONNECTION);
 const db = mongoose.connection;
 
 db.on('error', (error) => console.error(error));
