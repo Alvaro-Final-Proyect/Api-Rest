1d43e84acc9758d5d5901d2b3610467bdb4b9874
Alvaro <alvaromagu2002@gmail.com>
2022-06-08 10:12:51 +0200

    filter tournaments

--- components/tournaments/routes/getAllTournaments.js
+++ components/tournaments/routes/getAllTournaments.js
@@ -4,7 +4,14 @@ const populater = require('../settings/populater');
 const getAllTournaments = async (request, response) => {
     try{
         const tournaments = await Tournament.find().populate(populater);
-        return response.json(tournaments);
+
+        const currentDate = new Date(Date.now());
+        const limit = new Date();
+
+        limit.setDate(currentDate.getDate() - 3);
+
+        const filteredTournaments = tournaments.filter(tournament => tournament.final.date > limit);
+        return response.json(filteredTournaments);
     }catch(error){
         return response.status(500).json({ message: error.message });
     }
